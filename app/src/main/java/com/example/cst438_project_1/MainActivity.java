1c3134a3be8a69d51fe0969dbe0cb0d66fcba727
Pedro Muñoz <pmunoz@csumb.edu>
2021-02-04 13:20:34 -0800

    Commit to branch

--- app/src/main/java/com/example/cst438_project_1/MainActivity.java
+++ app/src/main/java/com/example/cst438_project_1/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String name = "Pedro";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
