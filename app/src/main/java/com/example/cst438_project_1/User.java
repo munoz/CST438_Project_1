a6f52345ad035afe315627cce1024ce551f2b833
Pedro Muñoz <pmunoz@csumb.edu>
2021-02-06 14:30:34 -0800

    Add some comments and Retrofit dependencies

--- app/src/main/java/com/example/cst438_project_1/User.java
+++ app/src/main/java/com/example/cst438_project_1/User.java
@@ -5,9 +5,14 @@ import androidx.room.PrimaryKey;
 
 import com.example.cst438_project_1.db.AppDatabase;
 
+/**
+ * This Java file is used with Room to save information to DB
+ */
+
 @Entity(tableName = AppDatabase.USER_TABLE)
 public class User {
 
+    //mUserID is a unique identifier, next like makes it automatically increment by one
     @PrimaryKey(autoGenerate = true)
     private int mUserId;
 
