83ae526a748fec1bfcce5285b8cff30fdfb2932c
Pedro Muñoz <pmunoz@csumb.edu>
2021-02-06 17:20:51 -0800

    - Fixed login bug - Added button to search on main activity - Corrected line 19 in SearchActivity.java because it was referencing the wrong layout xml

--- app/src/main/java/com/example/cst438_project_1/SearchActivity.java
+++ app/src/main/java/com/example/cst438_project_1/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.example.cst438_project_1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
@@ -8,10 +10,13 @@ import android.widget.Spinner;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String USER_ID_KEY = "com.example.cst438_project_1.userIdKey";
+    private static final String PREFERENCES_KEY = "com.example.cst438_project_1.PREFERENCES_KEY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_search);
 
         Spinner mySpinner = (Spinner) findViewById(R.id.typeResponse);
 
@@ -19,5 +24,12 @@ public class SearchActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.types));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+    }
+
+    public static Intent intentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, SearchActivity.class);
+        intent.putExtra(USER_ID_KEY, userId);
+
+        return intent;
     }
 }
