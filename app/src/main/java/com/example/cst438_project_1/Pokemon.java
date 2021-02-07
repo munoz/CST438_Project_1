eec29a6068b532e374f6731060098ac6503ccf10
Tim <tjrodgers49@gmail.com>
2021-02-06 20:10:43 -0800

    Created a Java class and interface. The class will hold the variables that will be retrieved from the json file. The interface is a json place holder for api retrievals.

---
+++ app/src/main/java/com/example/cst438_project_1/Pokemon.java
@@ -0,0 +1,35 @@
+package com.example.cst438_project_1;
+
+import java.util.List;
+
+public class Pokemon {
+    private String name;
+
+    private int id;
+
+    private List<String> types;
+
+    private int height;
+
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
