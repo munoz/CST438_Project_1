8070e19aad294f89d19e67c71e0f470a618323c7
Pedro Muñoz <pmunoz@csumb.edu>
2021-02-18 21:31:50 -0800

    Implemented Pokemon DB Add pokemon object to to DB

--- app/src/main/java/com/example/cst438_project_1/db/AppDatabase.java
+++ app/src/main/java/com/example/cst438_project_1/db/AppDatabase.java
@@ -3,12 +3,14 @@ package com.example.cst438_project_1.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.cst438_project_1.Pokemon;
 import com.example.cst438_project_1.User;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Pokemon.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DB_NAME = "POKEDEX_DATABASE";
     public static final String USER_TABLE = "USER_TABLE";
+    public static final String POKEMON_TABLE = "POKEMON_TABLE";
 
     public abstract PokedexDAO getPokedexDAO();
 
